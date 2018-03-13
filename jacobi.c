6b473942ccf25e6411b7543b10401fcfb9e4f594
richarz <richarz@wwu.edu>
2018-03-12 17:20:44 -0700

    matrixChanger function added. Does not compile

--- jacobi.c
+++ jacobi.c
@@ -5,44 +5,82 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define EPSILON 0.00001
 
+/*
+ */
+void changeChecker(double* maxChange, double CHALLENGER);
+
+/*
+ */
+void matrixChanger(double mtx1[1024][1024], double mtx2[1024][1024], int threadNum, int NumOfThreads, double* maxChange);
+
+/*fillMatrix
+ * fills a 1024x1024 2d array with values from a file named "input"
+ *  input: file to read values from.
+ *  mtx: empty 2d array to be filled.
+*/
 void fillMatrix(FILE* input, double mtx[1024][1024]);
 
 /* TO DO:
- * code jacobi
+ * debug jacobi
+ * mutex
  * find some way to time the program
- * add parallelization
+ * add parallelization:
+ *   mutex for maxChange
+ *   make matrix changes loop according to threadNum and NumOfThreads
  */
 int main(int argc, const char* argv[]) {
-  char* fileName = "jacobi";
+  char* fileName = "input.mtx";
   FILE* input = fopen(fileName, "r");
-  double mtx1[1024][1024];
+  double *mtx1;
+  mtx1 = (double *) malloc(1024*1024*sizeof(double));
   double mtx2[1024][1024];
   double maxChange = 0;
-  double epsilon = 0.00001;
+  int threadNum = 1;
+  int NumOfThreads = 1;
 
   fillMatrix(input, mtx1);
+  memcpy(mtx2, mtx1, sizeof(mtx1));
+  matrixChanger(mtx1, mtx2, threadNum, NumOfThreads, &maxChange);
 
-  while(maxChange > epsilon) {
-    for(int row = 1; row < 1023; row++) {
+}
+
+/*
+ */
+void changeChecker(double* maxChange, double CHALLENGER) {
+  //wait for opening
+  if(CHALLENGER > *maxChange) {
+    *maxChange = CHALLENGER;
+  }
+  //done
+}
+
+/*
+ */
+void matrixChanger(double mtx1[1024][1024], double mtx2[1024][1024], int threadNum, int NumOfThreads, double* maxChange) {
+
+  while(*maxChange > EPSILON) {
+    for(int row = threadNum; row < 1023; row+NumOfThreads) {
       for(int col = 1; col < 1023; col++) {
         mtx2[row][col] = mtx1[row-1][col] + mtx1[row][col-1] +
                          mtx1[row][col+1] + mtx1[row+1][col];
         mtx2[row][col] = mtx2[row][col]/4;
-        if(mtx2[row][col] > maxChange) {
-          maxChange = mtx2[row][col]-mtx1[row][col];
+        if(mtx2[row][col] > *maxChange) {
+          changeChecker(maxChange, mtx2[row][col]);
         }
       }
     }
 
-    if(maxChange <= epsilon) {
-      for(int row = 1; row < 1023; row++) {
+    if(*maxChange > EPSILON) {
+      for(int row = threadNum; row < 1023; row+NumOfThreads) {
         for(int col = 1; col < 1023; col++) {
           mtx1[row][col] = mtx2[row-1][col] + mtx2[row][col-1] +
                            mtx2[row][col+1] + mtx2[row+1][col];
           mtx1[row][col] = mtx1[row][col]/4;
-          if(mtx1[row][col] > maxChange) {
-            maxChange = mtx1[row][col]-mtx2[row][col];
+          if(mtx2[row][col] > *maxChange) {
+            changeChecker(maxChange, mtx2[row][col]);
           }
         }
       }
@@ -50,6 +88,17 @@ int main(int argc, const char* argv[]) {
   }
 }
 
+/*fillMatrix
+ * fills a 1024x1024 2d array with values from a file named "input"
+ * does so using three for loops
+ *
+ * The first for loop fills row 0, where every value must be read
+ *  from the file
+ * The second for loop fills rows 1-1022, where only columns 0 and 1023 must
+ *  be read from the file, and all other values can be set to 0
+ * The final for loop fills row 1023, where every value must be read
+ *  from the file
+ */
 void fillMatrix(FILE* input, double mtx[1024][1024]){
   int i = 0;
   int row = 0;
